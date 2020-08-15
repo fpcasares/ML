6fb8a02200b2b4ba03cfa92af9f3c2a94721bd19
Facundo Pablo Casares <fpcasares@gmail.com>
2020-08-15 19:44:30 -0300

    Update ML.py

--- ML.py
+++ ML.py
@@ -2,57 +2,110 @@ import requests
 import json
 import urllib.parse
 import sys
+import threading
 
 
 Product_Search_URL='https://api.mercadolibre.com/sites/MLA/search?category=MLA1246&q={}' #variable is product name as text
 Review_URL='https://api.mercadolibre.com/reviews/item/{}' #variable is the product ID
 Product_URL='https://api.mercadolibre.com/items/{}' #variable is the productID
 
-Product=sys.argv[1]
 
-Product=urllib.parse.quote(Product)
+class myThread (threading.Thread):
+   def __init__(self, threadID, product_id):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = product_id
+   def run(self):
+      get_image(self.product_id)
 
-r=requests.get(Product_Search_URL.format(Product))
+def print_time(threadName, delay, counter):
+   while counter:
+      if exitFlag:
+         threadName.exit()
+      time.sleep(delay)
+      print ("%s: %s" % (threadName, time.ctime(time.time())))
+      counter -= 1
 
-product_list=list()
+def get_image_by_product_id(product_id='MLA773625202'):
+    product_request=requests.get(Product_URL.format(product_id)
 
-if r.status_code == 200:
-    dict1=json.loads(r.text)
-    for item in dict1['results']:
-        product_item={'id':item['id'],'title':item['title'],'thumbnail':item['thumbnail'],'image':''}
-        product_list.append(product_item)
-
-    for item in product_list:
-        index=product_list.index(item)
-        r1=requests.get(Product_URL.format(item['id']))
-        if r1.status_code == 200:
-            response=json.loads(r1.text)
-            picture=response['pictures'][0]['url']
-            product_list[index]['image']=picture
-
-    for item in product_list:
-        print(item)
-
-
+    if product_request.status_code == 200:
+        response=json.loads(product_request.text)
+        picture=response['pictures'][0]['url']
+        return (picture)
 
 
 
 
-'''
 
 
-link=dict1['results'][0]['permalink']
+
+def get_products_by_name(product_name='kerastase'):
+    
+    product_name=urllib.parse.quote(product_name)
+    products_request=requests.get(Product_Search_URL.format(product_name))
+    
+    if products_request.status_code == 200:
+        product_list=list()
+        request_text=json.loads(r.text)
+        
+        for item in request_text['results']:
+            product_item={'id':item['id'],'title':item['title'],'thumbnail':item['thumbnail'],'image':''}
+            product_list.append(product_item)
+
+        for item in product_list:
+            index=product_list.index(item)
+            product_request=requests.get(Product_URL.format(item['id']))
+            if product_request.status_code == 200:
+                response=json.loads(product_request.text)
+                picture=response['pictures'][0]['url']
+                product_list[index]['image']=picture
+        return (product_list)
+
+def get_reviews_by_product_id(product_id='MLA773625202'):
+    review_request=requests.get(Review_URL.format(id))
+    
+    review_list=list()
+    if review_request.status_code == 200:
+        request_text=json.loads(review_request.text)
+        for item in request_text['reviews']:
+            review_item={'content':item['content'],'rate':item['rate']}
+            review_list.append(review_item)
+        return (review_list)
         
 
-id=dict1['results'][0]['id']
+    
 
-r=requests.get(Review_URL.format(id))
 
-if r.status_code == 200:
-    dict1=json.loads(r.text)
-    for item in dict1['reviews']:
-        print(item['content'])
-        print(item['rate'])
 
-'''
+
+
+Product=sys.argv[1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
