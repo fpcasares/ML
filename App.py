e424ef7dce61bdb3cd1b71dd3ad5c22ff196b78d
Facundo Pablo Casares <fpcasares@gmail.com>
2020-08-16 11:02:44 -0300

    Update App.py

--- App.py
+++ App.py
@@ -11,12 +11,12 @@ def hello_world():
 
 
 @app.route('/api/v1/get_products_by_name/<product_name>')
-def hello(product_name):
+def products_list(product_name):
     product_list=get_products_by_name(product_name)
     return render_template('products.j2', product_list=product_list)
     
 @app.route('/api/v1/get_reviews_by_product_id/<product_id>')
-def hello(product_id):
+def review_list(product_id):
     review_list=get_reviews_by_product_id(product_id)
     return render_template('reviews.j2', review_list=review_list)
  
