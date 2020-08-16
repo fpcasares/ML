c9a4757203347e10270fae0832bacc16e17a21c5
Facundo Pablo Casares <fpcasares@gmail.com>
2020-08-16 10:38:03 -0300

    Update App.py

--- App.py
+++ App.py
@@ -13,7 +13,7 @@ def hello_world():
 @app.route('/api/v1/get_product_by_name/<product_name>')
 def hello(product_name):
     product_list=get_products_by_name(product_name)
-    return render_template('app.j2', product_list=product_list)
+    return render_template('APP.j2', product_list=product_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5500')
