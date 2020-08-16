e0d6fba3245b1040d5b4714082e3ae5c531643da
Facundo Pablo Casares <fpcasares@gmail.com>
2020-08-16 10:27:20 -0300

    initializing...

--- App.py
+++ App.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from flask import render_template
+from ML import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/v1')
 def hello_world():
-    return 'Hello, World!'
+    return 'Welcome to OpinAPP initial version'
 
 
-@app.route('/hello/')
-def hello(name=None):
-    return render_template('app.j2', name=name)
+@app.route('/api/v1/get_product_by_name/<product_name>')
+def hello(product_name=product_name):
+    product_list=get_products_by_name(product_name)
+    return render_template('app.j2', product_list=product_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5500')
